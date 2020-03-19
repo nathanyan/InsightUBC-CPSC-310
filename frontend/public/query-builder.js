fb47cd702c058451e80418e0ed1c270d97cd1ce7
nyan604 <nyan604@students.cs.ubc.ca>
2020-03-19 00:09:48 -0700

    query-builder OPTIONS implemented and all passing

--- frontend/public/query-builder.js
+++ frontend/public/query-builder.js
@@ -29,14 +29,19 @@ CampusExplorer.buildQuery = function () {
     let where = {};
     let options = {};
     let columns = [];
-    let order = "";
+    let order = {};
     let transformations = {};
     let group = [];
     let apply = [];
     where = buildWhere(dataID);
     query["WHERE"] = where;
-    // options["COLUMNS"] = [];
-    // query["OPTIONS"] = options;
+    columns = buildColumns(dataID);
+    order = buildOrder(dataID);
+    options["COLUMNS"] = columns;
+    if (order !== "") {
+        options["ORDER"] = order;
+    }
+    query["OPTIONS"] = options;
     return query;
 };
 
@@ -128,4 +133,96 @@ function buildWhere(dataID) {
             }
         }
         return where;
+}
+
+function buildColumns(dataID) {
+    let columns = [];
+    let formElement;
+    if (dataID === "courses_") {
+        formElement = document.querySelector("form[data-type='courses']");  // choose appropriate form
+    }
+    if (dataID === "rooms_") {
+        formElement = document.querySelector("form[data-type='rooms']");
+    }
+    let formColumnsSection = formElement.querySelector("div.form-group.columns");  // grab only columns section
+    let columnsInputsAll = formColumnsSection.querySelectorAll("input");
+    let containsAtLeastOneCheckedColumn = false;
+    for (let columnInput of columnsInputsAll) {                     // check to see at least one column is checked
+        if (columnInput.getAttribute("checked") === "checked") {
+            containsAtLeastOneCheckedColumn = true;
+        }
+    }
+    if (!containsAtLeastOneCheckedColumn) {             // none checked, return empty []
+        return columns;
+    } else {
+        let columnsElementsChecked = formColumnsSection.querySelectorAll("input[checked='checked']");   // gather all input boxes checked
+        for (let columnElement of columnsElementsChecked) {         // for each checked input, grab the value (ie: "instructor") and (if needed) attach to the id string, then push to final list
+            let columnKeyField = columnElement.getAttribute("value");       // "instructor"
+            if (columnElement.parentNode.getAttribute("class") === "control transformation") {      // if parent is from a transformation apply key div, don't add id string to it
+                columns.push(columnKeyField);
+            } else {
+                let columnKey = dataID + columnKeyField;        // "courses_instructor"
+                columns.push(columnKey);
+            }
+        }
+        return columns;
+    }
+}
+
+function buildOrder(dataID) {
+    let orderSingle = "";
+    let orderMultiple = {};
+    let formElement;
+    if (dataID === "courses_") {
+        formElement = document.querySelector("form[data-type='courses']");  // choose appropriate form
+    }
+    if (dataID === "rooms_") {
+        formElement = document.querySelector("form[data-type='rooms']");
+    }
+    let formOrderSection = formElement.querySelector("div.form-group.order");  // grab only order section
+    let orderOptionsAll = formOrderSection.querySelectorAll("option");
+    let containsAtLeastOneSelectedOrder = false;
+    for (let orderOption of orderOptionsAll) {                     // check to see at least one order key is selected
+        if (orderOption.getAttribute("selected") === "selected") {
+            containsAtLeastOneSelectedOrder = true;
+        }
+    }
+    if (!containsAtLeastOneSelectedOrder) {                 // none selected, return empty ""
+        return orderSingle;
+    } else {
+        let orderOptionsSelected = formOrderSection.querySelectorAll("option[selected='selected']");   // gather all option elements with selected = "selected"
+        let descendingCheckbox
+        if (dataID === "courses_") {
+            descendingCheckbox = document.getElementById("courses-order");
+        } else {
+            descendingCheckbox = document.getElementById("rooms-order");
+        }
+        if (orderOptionsSelected.length === 1 && (descendingCheckbox.getAttribute("checked") !== "checked")) {      // only one selected and ascending, return 1 string
+            let orderKeyField = orderOptionsSelected.item(0).getAttribute("value");
+            if (orderOptionsSelected.item(0).getAttribute("class") === "transformation") {
+                orderSingle = orderKeyField;                          // check if applyKey, if so, then don't add ID to string
+                return orderSingle;
+            } else {
+                orderSingle = dataID + orderKeyField;                 // not an applyKey, must add ID to string
+                return orderSingle;
+            }
+        }
+        let orderKeys = [];
+        if (descendingCheckbox.getAttribute("checked") === "checked") {
+            orderMultiple["dir"] = "DOWN";
+        } else {
+            orderMultiple["dir"] = "UP";
+        }
+        for (let orderOption of orderOptionsSelected) {
+            let orderKeyField = orderOption.getAttribute("value");
+            if (orderOption.getAttribute("class") === "transformation") {
+                orderKeys.push(orderKeyField);
+            } else {
+                let orderKey = dataID + orderKeyField;        // "courses_instructor"
+                orderKeys.push(orderKey);
+            }
+        }
+        orderMultiple["keys"] = orderKeys;
+        return orderMultiple;
+    }
 }
